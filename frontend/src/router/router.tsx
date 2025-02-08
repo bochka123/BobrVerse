a9a66616aefaac7a5e6b297547c0e2276bbb3c64
Nadiia <78951348+double-hope@users.noreply.github.com>
2025-02-08 22:22:14 +0200

    add config, add AuthPage

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { AuthPage } from '@/pages';
+
 export const router = createBrowserRouter([
+    {
+        path: 'auth',
+        element: <AuthPage />,
+    },
     {
         path: '*',
         element: <h1>Page is not found</h1>,
