5193ec664c111eec9ce6836cc5a301ccc0e42ba8
YaroslavBugayov <bugayov576@gmail.com>
2025-02-11 01:22:20 +0200

    feature: add quest hints

---
+++ frontend/src/pages/quest/comonents/quest-hints/quest-hints.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+
+import { WoodenContainer } from '@/components';
+
+import styles from './quest-hints.module.scss';
+
+type QuestHintsProps = {}
+const QuestHints: FC<QuestHintsProps> = () => {
+    return (
+        <WoodenContainer className={styles.hintsContainer}>
+            <div className={styles.hintsWrapper}>
+                <h2>Objects</h2>
+                <ul>
+                    <li>tree</li>
+                    <li>binary tree</li>
+                </ul>
+            </div>
+            <div className={styles.hintsWrapper}>
+                <h2>Commands</h2>
+                <ul>
+                    <li>go</li>
+                    <li>play</li>
+                    <li>set</li>
+                </ul>
+            </div>
+        </WoodenContainer>
+    );
+};
+
+export { QuestHints };
