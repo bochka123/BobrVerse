658e820aa6d24eb194a1ca18b139e91e1c674a05
Oleksandr Tkach <oletk@smart-it.com>
2025-02-11 02:05:34 +0200

    added quest rating

---
+++ backend/BobrVerse.Dal/Entities/Quest/QuestRating.cs
@@ -0,0 +1,16 @@
+﻿namespace BobrVerse.Dal.Entities.Quest
+{
+    public class QuestRating
+    {
+        public Guid Id { get; set; }
+        public Guid? QuestId { get; set; }
+        public Quest? Quest { get; set; }
+        public Guid? BobrProfileId { get; set; }
+        public BobrProfile? BobrProfile { get; set; }
+        public Guid? QuestResponseId { get; set; }
+        public QuestResponse? QuestResponse { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public int Rating { get; set; }
+    }
+}
