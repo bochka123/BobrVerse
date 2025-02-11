d6d27b57ab02f6e86a200d3c22eef6415d9f7df5
YaroslavBugayov <bugayov576@gmail.com>
2025-02-11 20:29:53 +0200

    feature: refactor sign in page

--- frontend/src/components/primitives/inputs/base-input/base-input.tsx
+++ frontend/src/components/primitives/inputs/base-input/base-input.tsx
@@ -4,8 +4,10 @@ type BaseInputProps = {
     value: string;
     onChange: (value: string) => void;
     labelText?: string;
+    placeholder?: string;
+    type: string;
 }
-const BaseInput: FC<BaseInputProps> = ({ value, onChange, labelText }) => {
+const BaseInput: FC<BaseInputProps> = ({ value, onChange, labelText, placeholder, type }) => {
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
         onChange && onChange(e.target.value);
@@ -15,7 +17,7 @@ const BaseInput: FC<BaseInputProps> = ({ value, onChange, labelText }) => {
         <div>
             { labelText && <span>{labelText}</span>}
             <div>
-                <input value={value} onChange={handleChange} />
+                <input value={value} onChange={handleChange} placeholder={placeholder} type={type} />
             </div>
         </div>
     );
--- frontend/src/pages/auth/auth-page-form.tsx
+++ frontend/src/pages/auth/auth-page-form.tsx
@@ -3,7 +3,7 @@ import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 import { InputTypes } from '@/common';
-import { BaseButton } from '@/components';
+import { BaseButton, BaseInput } from '@/components';
 import { useAuth } from '@/hooks';
 import { IAuthRequestDto } from '@/models/requests';
 import { useLoginMutation, useRegisterMutation } from '@/services';
@@ -63,9 +63,7 @@ const AuthPageForm: FC<AuthPageFormProps> = ({ authType }) => {
                 name="email"
                 rules={{ required: 'Email field is required', pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }}
                 render={({ field: { onChange, value } }) => (
-                    <div className={styles.inputGroup}>
-                        <input value={value} onChange={onChange} type={InputTypes.EMAIL} placeholder="Email"/>
-                    </div>
+                    <BaseInput value={value} onChange={onChange} placeholder={'Email'} type={InputTypes.EMAIL}/>
                 )}
             />
             <Controller
@@ -73,17 +71,14 @@ const AuthPageForm: FC<AuthPageFormProps> = ({ authType }) => {
                 name="password"
                 rules={{ required: 'Password field is required' }}
                 render={({ field: { onChange, value } }) => (
-                    <div className={styles.inputGroup}>
-                        <input value={value} onChange={onChange} type={InputTypes.PASSWORD} placeholder="Password" />
-                    </div>
+                    <BaseInput value={value} onChange={onChange} placeholder={'Password'} type={InputTypes.PASSWORD}/>
                 )}
             />
-            <div className={styles.buttonsWrapper}>
-                <BaseButton type={'submit'} className={styles.authButton}>
-                    {authType === 'signIn' ? 'Login' : 'Register'}
-                </BaseButton>
-                <GoogleAuthButton/>
-            </div>
+
+            <BaseButton type={'submit'} className={styles.authButton}>
+                {authType === 'signIn' ? 'Login' : 'Register'}
+            </BaseButton>
+            <GoogleAuthButton/>
         </form>
     );
 };
