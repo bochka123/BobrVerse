dc073eac909cec23a4aab82b8cd61652ecdca1cc
YaroslavBugayov <bugayov576@gmail.com>
2025-02-12 15:02:20 +0200

    feature: finish add task modal

--- frontend/src/pages/quest-updating/components/quest-updating-layout/comonents/quest-slides/components/upsert-quest-task-modal/upsert-quest-task-modal.tsx
+++ frontend/src/pages/quest-updating/components/quest-updating-layout/comonents/quest-slides/components/upsert-quest-task-modal/upsert-quest-task-modal.tsx
@@ -3,15 +3,16 @@ import { Controller, SubmitErrorHandler, SubmitHandler, useForm } from 'react-ho
 
 import { InputTypes, TaskTypeEnum, ToastModeEnum } from '@/common';
 import { BaseButton, BaseInput, CheckboxInput, Modal, SelectInput } from '@/components';
-import { getFormErrorMessage } from '@/helpers';
+import { getFormErrorMessage, uuid } from '@/helpers';
 import { useToast } from '@/hooks';
-import { ICreateQuestTaskDto } from '@/models/requests';
+import { ICreateQuestTaskDto, IResourceDto } from '@/models/requests';
 import { useQuestUpdating } from '@/pages/quest-updating/hooks';
 import { useCreateQuestTaskMutation } from '@/services';
 
+import { RequiredResourcesInput } from './components';
 import styles from './upsert-quest-task-modal.module.scss';
 
-type FormNames = {
+type UpsertQuestTaskModalFormNames = {
     taskType: string;
     shortDescription: string;
     description: string;
@@ -19,6 +20,7 @@ type FormNames = {
     maxAttempts: number;
     timeLimitInSeconds: number;
     isTemplate: string;
+    requiredResources: IResourceDto[];
 }
 
 type UpsertQuestTaskModalProps = {
@@ -34,9 +36,13 @@ const UpsertQuestTaskModal: FC<UpsertQuestTaskModalProps> = ({ visible, setVisib
 
     const { addSlide } = useQuestUpdating();
 
-    const { handleSubmit, control } = useForm<FormNames>();
+    const { handleSubmit, control } = useForm<UpsertQuestTaskModalFormNames>({
+        defaultValues: {
+            requiredResources: [{ id: uuid(), name: 'Wood', quantity: 5, length: 10, weigth: 2 }],
+        }
+    });
 
-    const onSubmit: SubmitHandler<FormNames> = (data): void => {
+    const onSubmit: SubmitHandler<UpsertQuestTaskModalFormNames> = (data): void => {
         const requestData: ICreateQuestTaskDto = {
             questId: questId,
             isRequiredForNextStage: !!data.isRequiredForNextStage,
@@ -46,7 +52,7 @@ const UpsertQuestTaskModal: FC<UpsertQuestTaskModalProps> = ({ visible, setVisib
             taskType: data.taskType as TaskTypeEnum,
             shortDescription: data.shortDescription,
             description: data.description,
-            requiredResources: [],
+            requiredResources: data.requiredResources,
         };
 
         createTask(requestData)
@@ -62,104 +68,109 @@ const UpsertQuestTaskModal: FC<UpsertQuestTaskModalProps> = ({ visible, setVisib
             .catch(() => addToast(ToastModeEnum.ERROR, 'Failed to create quest'));
     };
 
-    const onError: SubmitErrorHandler<FormNames> = (error): void => {
+    const onError: SubmitErrorHandler<UpsertQuestTaskModalFormNames> = (error): void => {
         addToast(ToastModeEnum.ERROR, getFormErrorMessage(error));
     };
     
     return (
         <Modal visible={visible} setVisible={setVisible} heading={'Create quest task'}>
             <form onSubmit={handleSubmit(onSubmit, onError)} className={styles.form}>
-                <Controller
-                    control={control}
-                    name={'taskType'}
-                    rules={{ required: 'Task type field is required' }}
-                    render={({ field: { onChange, value } }) => (
-                        <SelectInput
-                            labelText={'Task type:'}
-                            value={value}
-                            onChange={onChange}
-                            options={Object.entries(TaskTypeEnum).map(([_, value]) => value )}
+                <div className={styles.inputsWrapper}>
+                    <div className={styles.baseInputsWrapper}>
+                        <Controller
+                            control={control}
+                            name={'taskType'}
+                            rules={{ required: 'Task type field is required' }}
+                            render={({ field: { onChange, value } }) => (
+                                <SelectInput
+                                    labelText={'Task type:'}
+                                    value={value}
+                                    onChange={onChange}
+                                    options={Object.entries(TaskTypeEnum).map(([_, value]) => value )}
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Controller
-                    control={control}
-                    name={'shortDescription'}
-                    rules={{ required: 'Short description field is required' }}
-                    render={({ field: { onChange, value } }) => (
-                        <BaseInput
-                            labelText={'Short description:'}
-                            value={value}
-                            onChange={onChange}
-                            placeholder={'Enter short description...'}
+                        <Controller
+                            control={control}
+                            name={'shortDescription'}
+                            rules={{ required: 'Short description field is required' }}
+                            render={({ field: { onChange, value } }) => (
+                                <BaseInput
+                                    labelText={'Short description:'}
+                                    value={value}
+                                    onChange={onChange}
+                                    placeholder={'Enter short description...'}
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Controller
-                    control={control}
-                    name={'description'}
-                    rules={{ required: 'Description field is required' }}
-                    render={({ field: { onChange, value } }) => (
-                        <BaseInput
-                            labelText={'Description:'}
-                            value={value}
-                            onChange={onChange}
-                            placeholder={'Enter description...'}
+                        <Controller
+                            control={control}
+                            name={'description'}
+                            rules={{ required: 'Description field is required' }}
+                            render={({ field: { onChange, value } }) => (
+                                <BaseInput
+                                    labelText={'Description:'}
+                                    value={value}
+                                    onChange={onChange}
+                                    placeholder={'Enter description...'}
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Controller
-                    control={control}
-                    name={'maxAttempts'}
-                    rules={{ required: 'Max attempts field is required' }}
-                    render={({ field: { onChange, value } }) => (
-                        <BaseInput
-                            labelText={'Max attempts:'}
-                            value={value}
-                            onChange={onChange}
-                            placeholder={'Enter max attempts...'}
-                            type={InputTypes.NUMBER}
-                            min={0}
+                        <Controller
+                            control={control}
+                            name={'maxAttempts'}
+                            rules={{ required: 'Max attempts field is required' }}
+                            render={({ field: { onChange, value } }) => (
+                                <BaseInput
+                                    labelText={'Max attempts:'}
+                                    value={value}
+                                    onChange={onChange}
+                                    placeholder={'Enter max attempts...'}
+                                    type={InputTypes.NUMBER}
+                                    min={0}
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Controller
-                    control={control}
-                    name={'timeLimitInSeconds'}
-                    rules={{ required: 'Time limit in seconds field is required' }}
-                    render={({ field: { onChange, value } }) => (
-                        <BaseInput
-                            labelText={'Time limit in seconds:'}
-                            value={value}
-                            onChange={onChange}
-                            placeholder={'Enter time limit in seconds...'}
-                            type={InputTypes.NUMBER}
-                            min={0}
+                        <Controller
+                            control={control}
+                            name={'timeLimitInSeconds'}
+                            rules={{ required: 'Time limit in seconds field is required' }}
+                            render={({ field: { onChange, value } }) => (
+                                <BaseInput
+                                    labelText={'Time limit in seconds:'}
+                                    value={value}
+                                    onChange={onChange}
+                                    placeholder={'Enter time limit in seconds...'}
+                                    type={InputTypes.NUMBER}
+                                    min={0}
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Controller
-                    control={control}
-                    name={'isRequiredForNextStage'}
-                    render={({ field: { onChange, value } }) => (
-                        <CheckboxInput
-                            labelText={'Is required for next stage:'}
-                            value={value}
-                            onChange={onChange}
+                        <Controller
+                            control={control}
+                            name={'isRequiredForNextStage'}
+                            render={({ field: { onChange, value } }) => (
+                                <CheckboxInput
+                                    labelText={'Is required for next stage:'}
+                                    value={value}
+                                    onChange={onChange}
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Controller
-                    control={control}
-                    name={'isTemplate'}
-                    render={({ field: { onChange, value } }) => (
-                        <CheckboxInput
-                            labelText={'Is template:'}
-                            value={value}
-                            onChange={onChange}
+                        <Controller
+                            control={control}
+                            name={'isTemplate'}
+                            render={({ field: { onChange, value } }) => (
+                                <CheckboxInput
+                                    labelText={'Is template:'}
+                                    value={value}
+                                    onChange={onChange}
+                                />
+                            )}
                         />
-                    )}
-                />
+                    </div>
+                    <RequiredResourcesInput control={control} />
+                </div>
 
                 <BaseButton type={'submit'}>
                     Create
@@ -169,4 +180,4 @@ const UpsertQuestTaskModal: FC<UpsertQuestTaskModalProps> = ({ visible, setVisib
     );
 };
 
-export { UpsertQuestTaskModal };
+export { UpsertQuestTaskModal, type UpsertQuestTaskModalFormNames };
