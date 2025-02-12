db68d5a60a58b8d5689d6dab5bf042e0c425c86f
Nadiia <78951348+double-hope@users.noreply.github.com>
2025-02-12 04:14:07 +0200

    add signalR

---
+++ backend/BobrVerse.Api/Hubs/AuthHubFilter.cs
@@ -0,0 +1,46 @@
+﻿using BobrVerse.Common.Exceptions;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BobrVerse.Api.Hubs
+{
+    public class AuthHubFilter : IHubFilter
+    {
+        public async ValueTask<object> InvokeMethodAsync(
+            HubInvocationContext invocationContext,
+            Func<HubInvocationContext, ValueTask<object>> next
+        )
+        {
+            var expiryClaim = invocationContext.Context.User.Claims.FirstOrDefault(x => x.Type == "exp").Value;
+            //if (string.IsNullOrEmpty(expiryClaim) || !long.TryParse(expiryClaim, out var expiryUnixTimestamp))
+            //{
+            //    throw new BobrException("Invalid or missing 'exp' claim.");
+            //}
+
+            //var expiryDate = DateTimeOffset.FromUnixTimeSeconds(expiryUnixTimestamp);
+
+            //if (DateTimeOffset.UtcNow >= expiryDate)
+            //{
+            //    throw new BobrException("auth_expired");
+            //}
+
+            return await next(invocationContext);
+        }
+
+        public Task OnConnectedAsync(
+            HubLifetimeContext context,
+            Func<HubLifetimeContext, Task> next
+        )
+        {
+            return next(context);
+        }
+
+        public Task OnDisconnectedAsync(
+            HubLifetimeContext context,
+            Exception exception,
+            Func<HubLifetimeContext, Exception, Task> next
+        )
+        {
+            return next(context, exception);
+        }
+    }
+}
---
+++ backend/BobrVerse.Api/Hubs/QuestsHub.cs
@@ -0,0 +1,50 @@
+﻿using BobrVerse.Api.Services;
+using BobrVerse.Auth.Attributes;
+using BobrVerse.Auth.Entities;
+using BobrVerse.Auth.Interfaces;
+using BobrVerse.Auth.Models.Settings;
+using BobrVerse.Common.Models.DTO.Quest;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BobrVerse.Api.Hubs
+{
+    [MainAuth]
+    public class QuestsHub(AuthSettings authSettings, IUserContextService userContextService, IAccessTokenService accessTokenService) : Hub
+    {
+        private readonly static ConnectionMapping<string> _connections = new();
+        private readonly CookieSettings cookieSettings = authSettings.Cookie;
+
+        public static IEnumerable<string> GetConnections(string userClaim)
+        {
+            return _connections.GetConnections(userClaim);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            if (httpContext != null)
+            {
+                var accessToken = httpContext.Request.Cookies[cookieSettings.AccessTokenName];
+
+                if (accessTokenService.TryValidateAccessToken(accessToken, out var claimsPrincipal))
+                {
+                    var userId = claimsPrincipal?.FindFirst(nameof(User))?.Value;
+                    _connections.Add(userId, Context.ConnectionId);
+                }
+            }
+
+            return base.OnConnectedAsync();
+        }
+
+        public async Task NotifyCreated(QuestDTO quest, string userClaim)
+        {
+            if (_connections.GetConnections(userClaim) is IEnumerable<string> connectionIds)
+            {
+                foreach (var connectionId in connectionIds)
+                {
+                    await Clients.Client(connectionId).SendAsync("questCreated", quest);
+                }
+            }
+        }
+    }
+}
