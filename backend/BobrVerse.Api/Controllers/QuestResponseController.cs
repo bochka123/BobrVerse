f355e0e0cc6b7d5759fb639cf1c408e22bfa0fb7
Oleksandr Tkach <oletk@smart-it.com>
2025-02-12 16:38:18 +0200

    fix

--- backend/BobrVerse.Api/Controllers/QuestResponseController.cs
+++ backend/BobrVerse.Api/Controllers/QuestResponseController.cs
@@ -1,5 +1,6 @@
 ﻿using BobrVerse.Auth.Attributes;
 using BobrVerse.Bll.Interfaces.Quest;
+using BobrVerse.Common.Models.Api;
 using BobrVerse.Common.Models.DTO.Quest;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +12,11 @@ namespace BobrVerse.Api.Controllers
     public class QuestResponseController(IQuestResponseService service) : ControllerBase
     {
         [HttpPost("create")]
-        public async Task<QuestResponseDTO> Create(Guid questId) 
-            => await service.CreateAsync(questId);
+        public async Task<ApiResponse<QuestResponseDTO>> Create(Guid questId) 
+            => new ApiResponse<QuestResponseDTO>(await service.CreateAsync(questId));
 
         [HttpGet("getUserQuestResponses/{start:int}/{end:int}")]
-        public async Task<ICollection<QuestResponseDTO>> GetUserQuestResponses([FromRoute] int start, [FromRoute] int end)
-            => await service.GetUserQuestResponsesAsync(start, end);
+        public async Task<ApiResponse<ICollection<QuestResponseDTO>>> GetUserQuestResponses([FromRoute] int start, [FromRoute] int end)
+            => new ApiResponse<ICollection<QuestResponseDTO>>(await service.GetUserQuestResponsesAsync(start, end));
     }
 }
