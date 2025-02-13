baa7ae9c86c7943cf42b97037c32970677fbee2d
YaroslavBugayov <bugayov576@gmail.com>
2025-02-13 02:31:34 +0200

    feature: AAA

---
+++ backend/BobrVerse.Dal/Migrations/20250213002554_AddCurrentAttempt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BobrVerse.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrentAttempt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentAttempt",
+                table: "QuizTaskStatuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentAttempt",
+                table: "QuizTaskStatuses");
+        }
+    }
+}
